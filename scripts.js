677361b07665aeb10e3a81b14dbb2b8bf1a1a96e
James Arn <132308884+arnsocks@users.noreply.github.com>
2024-09-01 17:23:20 -0400

    Create the 16x16 grid of squares

--- scripts.js
+++ scripts.js
@@ -1,2 +1,17 @@
-const body = document.querySelector("body");
-body.style.backgroundColor = "yellow";
+const gridContainer = document.querySelector("#container");
+
+initializeGrid(16, 16);
+
+function initializeGrid(width, height) {
+  for(let i = 0; i < height; i++) {
+    const row = document.createElement("div");
+    row.className = "row";
+    for (let j = 0; j < width; j++) {
+      const square = document.createElement("div");
+      square.className = "square";
+      row.appendChild(square);
+    }
+    gridContainer.appendChild(row);
+
+  }
+}
